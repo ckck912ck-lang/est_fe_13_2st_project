c3ccf60c58331912ae335b44d6362ccd3a19badb
Raspbsb <unionbjh@naver.com>
2026-06-15 09:09:55 +0900

    refactor: 폴더 구조 영역 세분화

---
+++ js/modules/testimonial.js
@@ -0,0 +1,6 @@
+// renderStars.js에서 renderStars 함수를 import해 후기 별점 렌더링에 사용
+// reviews(데이터) 배열, container(HTML이 들어갈 태그), maxCount(변수)를 매개변수로 받음
+// reviews 배열에서 최대 maxCount개만큼 후기 데이터를 선택
+// 각 후기의 rating 값은 renderStars(rating)에 전달해 별점 HTML 생성
+// 후기의 작성자, 작성일, 내용 값을 사용해 후기 카드 HTML 생성
+// 생성한 후기 카드를 frag에 모은 뒤 container에 HTML로 생성
