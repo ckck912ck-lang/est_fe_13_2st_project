cc6ee1473c56fb3bf2f3082315ccda0a8a2cec34
Raspbsb <unionbjh@naver.com>
2026-06-16 17:40:10 +0900

    feat: renderProducts.js 상품생성 반복 후 HTML 렌더링

--- js/modules/renderProducts.js
+++ js/modules/renderProducts.js
@@ -1,5 +1,13 @@
+import { renderProductCard } from "/js/modules/renderProductCard.js";
+
 // renderProductCard.js를 import해 상품 카드 HTML 생성
-// 렌더링할 products 배열(전체 번호 중 페이지네이션으로 걸러진 n개 배열)과 container(HTML이 들어갈 태그)를 매개변수로 받음
-// 전달받은 products 배열만 순회해 상품 카드 목록 생성
+// 렌더링할 filteredProducts 배열(전체 번호 중 페이지네이션으로 걸러진 n개 배열)과 container(HTML이 들어갈 태그)를 매개변수로 받음
+// 전달받은 filteredProducts 배열만 순회해 상품 카드 목록 생성
 // 기존 container 내용을 비우고 생성한 상품 카드를 HTML로 출력
 // 페이지네이션 계산은 productList.js에서 처리
+
+export function renderProducts(filteredProducts, container) {
+  const html = filteredProducts.map((p) => renderProductCard(p));
+
+  container.innerHTML = html.join("");
+}
