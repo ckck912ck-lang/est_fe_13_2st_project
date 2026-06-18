2fd1f0dd49bee791d52d140e61fae741412b2201
주성문 <enforhssh@gmail.com>
2026-06-18 15:03:22 +0900

    refactor:signup.html 헤더, 푸터 모듈화

--- js/pages/signup.js
+++ js/pages/signup.js
@@ -1,3 +1,12 @@
+import { renderHeader } from "../modules/renderHeader.js";
+import { renderFooter } from "../modules/renderFooter.js";
+import { initSearch } from "../modules/search.js";
+
+// 공용 모듈 불러오기
+renderHeader("");
+renderFooter();
+initSearch();
+
 // 회원가입 기능
 
 // 비밀번호 보기/점으로 표시
