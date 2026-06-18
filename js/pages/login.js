931ccae68e760d534a71bddd13c8055291f21bb2
Raspbsb <unionbjh@naver.com>
2026-06-18 17:45:29 +0900

    feat: 햄버거, 고정버튼, 문의모달창 HTML, 모듈 import

--- js/pages/login.js
+++ js/pages/login.js
@@ -1,5 +1,9 @@
 import { renderHeader } from "../modules/renderHeader.js";
 import { renderFooter } from "../modules/renderFooter.js";
+import { openCloseHamburger, renderHamburger } from "../modules/hamburgerNav.js";
+import { renderChatting, openChattingModal, closeChattingModal } from "../modules/fixedBtn.js";
+
+const fixedBtn = document.querySelector(".fixed-chat-button");
 
 // 공용 모듈 불러오기
 renderHeader("C");
@@ -11,3 +15,19 @@ renderFooter();
 
 // 실시간으로 이메일/비밀번호 형식 피드백
 
+// 문의 모달 렌더링
+renderChatting();
+
+// 문의 고정버튼을 누르면 문의 모달창을 여는 함수
+openChattingModal(fixedBtn);
+
+// 문의 모달창의 닫기 버튼을 누르면 문의 모달창을 닫는 함수
+closeChattingModal();
+
+// 햄버거 렌더링
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+renderHamburger(hamburgerMenu);
+
+// 햄버거 열기
+const openHamburger = document.querySelector(".hamburger-btn-open");
+openCloseHamburger(openHamburger);
