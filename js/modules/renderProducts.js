de5b138db9cb13f9646a1ecf4386b2629d1954c4
Raspbsb <unionbjh@naver.com>
2026-06-18 15:39:43 +0900

    fix: 루트경로 -> 상대경로 수정

--- js/modules/renderProducts.js
+++ js/modules/renderProducts.js
@@ -1,6 +1,6 @@
 // renderProducts : 배정호 작업
 
-import { renderProductCard } from "/js/modules/renderProductCard.js";
+import { renderProductCard } from "./renderProductCard.js";
 
 // renderProductCard.js를 import해 상품 카드 HTML 생성
 // 렌더링할 filteredProducts 배열(전체 번호 중 페이지네이션으로 걸러진 n개 배열)과 container(HTML이 들어갈 태그)를 매개변수로 받음
