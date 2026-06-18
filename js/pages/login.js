02c38c3dbcc432c49f5faefd7f3cd91f34f2e76c
주성문 <enforhssh@gmail.com>
2026-06-18 15:01:33 +0900

    refactor:login.html 헤더, 푸터 모듈화

--- js/pages/login.js
+++ js/pages/login.js
@@ -1,5 +1,15 @@
+import { renderHeader } from "../modules/renderHeader.js";
+import { renderFooter } from "../modules/renderFooter.js";
+import { initSearch } from "../modules/search.js";
+
+// 공용 모듈 불러오기
+renderHeader("");
+renderFooter();
+initSearch();
+
 // 로그인 기능
 
 // 비밀번호 보기/점으로 표시
 
 // 실시간으로 이메일/비밀번호 형식 피드백
+
